c8cfc376a205939a0e2f8a4c4db3548e243d6044
sp <2849872658@qq.com>
2024-03-30 15:54:23 +0800

    feat: 添加eslint功能

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -12,19 +12,6 @@ module.exports = {
     "subject-full-stop": [2, "never", "."],
     "type-case": [2, "always", "lower-case"],
     "type-empty": [2, "never"],
-    "type-enum": [
-      2,
-      "always",
-      [
-        "feat", //添加功能
-        "fix", //修改bug
-        "docs", //修改文档
-        "style", //修改样式
-        "test", //添加测试
-        "refactor", //功能重构
-        "chore", //其他功能
-        "revert", //恢复设置
-      ],
-    ],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'test', 'refactor', 'chore', 'revert']],
   },
 };
