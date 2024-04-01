23c73ae407ec25dd02025737c61ac5d60ebb264c
sp <2849872658@qq.com>
2024-04-02 00:41:12 +0800

    feat: 实现了一件扫描，修改功能

--- packages/blackworld-lint/test/index.test.js
+++ packages/blackworld-lint/test/index.test.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const fs = require('fs-extra');
-const encodeFeLint = require('../lib/index');
+const blackworldFeLint = require('../lib/index');
 
-const { init } = encodeFeLint;
+const { init } = blackworldFeLint;
 
 describe('init', () => {
   const templatePath = path.resolve(__dirname, './fixtures/template/init');
